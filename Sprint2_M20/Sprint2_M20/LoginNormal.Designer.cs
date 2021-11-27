8b1c3ff280fbf225fe33462941ecb24900292aa6
abeel360 <abel.rodriguez.sds@gmail.com>
2021-11-27 05:08:44 +0100

    m

--- Sprint2_M20/Sprint2_M20/LoginNormal.Designer.cs
+++ Sprint2_M20/Sprint2_M20/LoginNormal.Designer.cs
@@ -31,9 +31,9 @@ namespace Sprint2_M20
         {
             this.txtUser = new System.Windows.Forms.TextBox();
             this.txtPass = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblUsuari = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtUser
@@ -51,47 +51,48 @@ namespace Sprint2_M20
             this.txtPass.TabIndex = 1;
             this.txtPass.UseSystemPasswordChar = true;
             // 
-            // label1
+            // lblUsuari
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Usuari";
+            this.lblUsuari.AutoSize = true;
+            this.lblUsuari.Location = new System.Drawing.Point(38, 37);
+            this.lblUsuari.Name = "lblUsuari";
+            this.lblUsuari.Size = new System.Drawing.Size(49, 17);
+            this.lblUsuari.TabIndex = 2;
+            this.lblUsuari.Text = "Usuari";
             // 
-            // label2
+            // lblPass
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 79);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Pass";
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(38, 79);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(39, 17);
+            this.lblPass.TabIndex = 3;
+            this.lblPass.Text = "Pass";
             // 
-            // button1
+            // btnLogin
             // 
-            this.button1.Location = new System.Drawing.Point(118, 172);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 41);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnLogin.Location = new System.Drawing.Point(118, 172);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 41);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
             // 
             // LoginNormal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(285, 271);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUsuari);
             this.Controls.Add(this.txtPass);
             this.Controls.Add(this.txtUser);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "LoginNormal";
             this.Text = "LoginNormal";
+            this.Load += new System.EventHandler(this.LoginNormal_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,8 +102,8 @@ namespace Sprint2_M20
 
         private System.Windows.Forms.TextBox txtUser;
         private System.Windows.Forms.TextBox txtPass;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblUsuari;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
--- Sprint2_M20/Sprint2_M20/LoginNormal.cs
+++ Sprint2_M20/Sprint2_M20/LoginNormal.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Xml;
+using System.IO;
 
 namespace Sprint2_M20
 {
@@ -17,6 +21,10 @@ namespace Sprint2_M20
         {
             InitializeComponent();
         }
+
+        string MAC;
+        LibreriaM20.LibreriaMESSI bd = new LibreriaM20.LibreriaMESSI();
+        DataSet dts;
         int contador = 0;
         private void button1_Click(object sender, EventArgs e)
         {
@@ -41,6 +49,42 @@ namespace Sprint2_M20
                     PantallaInicio frmPantallaInicio = new PantallaInicio();
                     frmPantallaInicio.ShowDialog();
                 }
+        }
+
+        private void LoginNormal_Load(object sender, EventArgs e)
+        {
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // Only consider Ethernet network interfaces
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet &&
+                    adapter.OperationalStatus == OperationalStatus.Up)
+                {
+                    PhysicalAddress address = adapter.GetPhysicalAddress();
+                    byte[] bytes = address.GetAddressBytes();
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        // Convierte la direccion en hexadeximal
+                        MAC = String.Format("{0}", bytes[i].ToString("X2"));
+                        // Inserta un guion entre cada byte
+                        if (i != bytes.Length - 1)
+                        {
+                            address += "-";
+                        }
+                    }
+                }
+            }
+
+            Dns.GetHostName();
+
+
+            dts = bd.TraerDatos("Users", "Select codeUser, password from Users;");
+            DataTable dtUsers = dts.Tables[0];
+
+            dts = bd.TraerDatos("Users", "Select codeUser, password from Users;");
+            DataTable dtDevices = dts.Tables[0];
         }
     }
 }
